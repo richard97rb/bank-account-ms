bae43969c56d7ad8a7ab7b06a0f01c13087c6184
Tony Abel Sanchez Escalante <tsanchee@everis.com>
2021-10-26 01:13:52 -0500

    ajustes de seervicios

--- src/main/java/com/bank/account/document/Account.java
+++ src/main/java/com/bank/account/document/Account.java
@@ -1,6 +1,8 @@
 package com.bank.account.document;
 
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,7 +18,12 @@ public class Account {
 	private float initialSAmount;
 	private float currentAmount;
 	private boolean isActive;
-	
+	private int maxMonthtransactions;
+	private int currentTransactions;
+	private Date transactionDay;
+	private double maxMonthCredit;
+	private boolean isLimited;
+	private double commission;
 	public String getId() {
 		return id;
 	}
@@ -65,8 +72,45 @@ public class Account {
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
 	}
+	public int getMaxMonthtransactions() {
+		return maxMonthtransactions;
+	}
+	public void setMaxMonthtransactions(int maxMonthtransactions) {
+		this.maxMonthtransactions = maxMonthtransactions;
+	}
+	public int getCurrentTransactions() {
+		return currentTransactions;
+	}
+	public void setCurrentTransactions(int currentTransactions) {
+		this.currentTransactions = currentTransactions;
+	}
+	public Date getTransactionDay() {
+		return transactionDay;
+	}
+	public void setTransactionDay(Date transactionDay) {
+		this.transactionDay = transactionDay;
+	}
+	public double getMaxMonthCredit() {
+		return maxMonthCredit;
+	}
+	public void setMaxMonthCredit(double maxMonthCredit) {
+		this.maxMonthCredit = maxMonthCredit;
+	}
+	public boolean isLimited() {
+		return isLimited;
+	}
+	public void setLimited(boolean isLimited) {
+		this.isLimited = isLimited;
+	}
+	public double getCommission() {
+		return commission;
+	}
+	public void setCommission(double commission) {
+		this.commission = commission;
+	}
 	public Account(String id, String personId, String accountType, String accountNumber, String currency,
-			float initialSAmount, float currentAmount, boolean isActive) {
+			float initialSAmount, float currentAmount, boolean isActive, int maxMonthtransactions,
+			int currentTransactions, Date transactionDay, double maxMonthCredit, boolean isLimited, double commission) {
 		super();
 		this.id = id;
 		this.personId = personId;
@@ -76,6 +120,12 @@ public class Account {
 		this.initialSAmount = initialSAmount;
 		this.currentAmount = currentAmount;
 		this.isActive = isActive;
+		this.maxMonthtransactions = maxMonthtransactions;
+		this.currentTransactions = currentTransactions;
+		this.transactionDay = transactionDay;
+		this.maxMonthCredit = maxMonthCredit;
+		this.isLimited = isLimited;
+		this.commission = commission;
 	}
 	public Account() {
 		super();
@@ -84,7 +134,11 @@ public class Account {
 	public String toString() {
 		return "Account [id=" + id + ", personId=" + personId + ", accountType=" + accountType + ", accountNumber="
 				+ accountNumber + ", currency=" + currency + ", initialSAmount=" + initialSAmount + ", currentAmount="
-				+ currentAmount + ", isActive=" + isActive + "]";
+				+ currentAmount + ", isActive=" + isActive + ", maxMonthtransactions=" + maxMonthtransactions
+				+ ", currentTransactions=" + currentTransactions + ", transactionDay=" + transactionDay
+				+ ", maxMonthCredit=" + maxMonthCredit + ", isLimited=" + isLimited + ", commission=" + commission
+				+ "]";
 	}
+	
 	
 }
--- src/main/java/com/bank/account/service/impl/AccountServiceImpl.java
+++ src/main/java/com/bank/account/service/impl/AccountServiceImpl.java
@@ -1,17 +1,21 @@
 package com.bank.account.service.impl;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.mongodb.core.aggregation.ConditionalOperators.Switch;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.bank.account.constant.AccountType;
+import com.bank.account.constant.PersonType;
 import com.bank.account.document.Account;
 import com.bank.account.repository.AccountRepository;
 import com.bank.account.service.AccountService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class AccountServiceImpl implements AccountService{
@@ -23,20 +27,53 @@ public class AccountServiceImpl implements AccountService{
 	public List<Account> getAccounts() {
 		return accountRepository.findAll();
 	}
-
-	public Account saveAccount(Account account) {
+	
+	public String getPersonType(String personId){
+		String personType="";
+		ObjectMapper mapper = new ObjectMapper();
 		RestTemplate restTemplate = new RestTemplate();
-	    String result = restTemplate.getForObject(personEndpoint, String.class, account.getPersonId());
-	    System.out.println(result);
+	    String result = restTemplate.getForObject(personEndpoint, String.class, personId);
+	    try {
+	    	Map<String, String> map = mapper.readValue(result, Map.class);
+	    	personType=map.get("personType");
+		} catch (IOException e) {
+            e.printStackTrace();
+		}
+	    return personType;
+	}
+	
+	public Account saveAccount(Account account) {
+		Account newAccount= null;
+		String personType=getPersonType(account.getPersonId());
+		
 		if(account.getAccountType().equals(AccountType.CUENTA_AHORRO.toString())){
-			
+			account.setCommission(0);
+			//account.setMaxMonthtransactions(10);
 		} else if(account.getAccountType().equals(AccountType.CUENTA_CORRIENTE.toString())){
-			
+			//account.setCommission(5);
+			account.setLimited(false);
 		} else if(account.getAccountType().equals(AccountType.PLAZO_FIJO.toString())){
-			
+			account.setCommission(0);
+			account.setMaxMonthtransactions(1);
+			//account.setTransactionDay(new Date("2021-10-28"));
 		}
 		
-		return accountRepository.save(account);
+		List<Account> accts = accountRepository.findByPersonId(account.getPersonId());
+		
+		if (personType.equals(PersonType.PERSONAL.toString())) {
+			if (accts==null || accts.size()==0) {
+				newAccount = accountRepository.save(account);
+			}else {
+				System.out.println("El Cliente ya tiene una cuenta");
+			}
+		}else if (personType.equals(PersonType.EMPRESARIAL.toString()))  {
+			if (account.getAccountType().equals(AccountType.CUENTA_CORRIENTE.toString())) {
+				newAccount = accountRepository.save(account);
+			}else {
+				System.out.println("El Cliente solo puede tener cuenta corriente");
+			}
+		}
+		return newAccount;
 	}
 
 	public Account editAccount(Account account) {
